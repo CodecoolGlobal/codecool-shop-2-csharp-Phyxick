74e16293e4d74a8ea8a771dc3b4a76b779321364
jjanos139 <jdale139@gmail.com>
2022-05-11 21:20:49 +0200

    Refactor controller and view (login generalized to user)

--- src/Codecool.CodecoolShop/Controllers/UserController.cs
+++ src/Codecool.CodecoolShop/Controllers/UserController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using Codecool.CodecoolShop.Daos.Implementations;
 using Codecool.CodecoolShop.Helpers;
 using Codecool.CodecoolShop.Models;
@@ -9,14 +10,14 @@ using Microsoft.Extensions.Logging;
 
 namespace Codecool.CodecoolShop.Controllers
 {
-    public class LoginController : Controller
+    public class UserController : Controller
     {
-        private readonly ILogger<LoginController> _logger;
+        private readonly ILogger<UserController> _logger;
         public UserService UserService { get; set; }
 
         EmailSender emailSender = new EmailSender();
 
-        public LoginController(ILogger<LoginController> logger)
+        public UserController(ILogger<UserController> logger)
         {
             _logger = logger;
             UserService = new UserService(UserDaoDB.GetInstance());
@@ -44,7 +45,7 @@ namespace Codecool.CodecoolShop.Controllers
             }
 
             var message = "Please enter the correct credentials!";
-            return RedirectToAction("Index", "Login", new {message = message});
+            return RedirectToAction("Index", "User", new {message = message});
         }
 
         public IActionResult Register()
@@ -59,13 +60,42 @@ namespace Codecool.CodecoolShop.Controllers
                 return RedirectToAction("Index", "Product");
             }
             var message = "User already exists!";
-            return (RedirectToAction("Index", "Login", new { message = message }));
+            return (RedirectToAction("Index", "User", new { message = message }));
         }
 
         public IActionResult Logout()
         {
             HttpContext.Session.Remove("username");
             return RedirectToAction("Index", "Product");
+        }
+
+        public IActionResult Details()
+        {
+            string username = HttpContext.Session.GetString("username")?.Replace("\"", "");
+            ViewData["user"] = UserService.GetUserData(username);
+            return View();
+        }
+
+        public IActionResult UpdateUserData()
+        {
+            string username = HttpContext.Session.GetString("username")?.Replace("\"", "");
+            var user = UserService.GetUserData(username);
+            user.Name = Request.Form["Name"];
+            user.Email = Request.Form["Email"];
+            user.Phone = Request.Form["Phone number"];
+            user.BillingCountry = Request.Form["Billing Country"];
+            user.BillingZipcode = Request.Form["Billing Zipcode"];
+            user.BillingCity = Request.Form["Billing City"];
+            user.BillingStreet = Request.Form["Billing Street"];
+            user.BillingHouseNumber = Request.Form["Billing House"];
+            user.ShippingCountry = Request.Form["Shipping Country"];
+            user.ShippingZipcode = Request.Form["Shipping Zipcode"];
+            user.ShippingCity = Request.Form["Shipping City"];
+            user.ShippingStreet = Request.Form["Shipping Street"];
+            user.ShippingHouseNumber = Request.Form["Shipping House"];
+            UserService.UpdateUserData(user);
+            ViewData["user"] = UserService.GetUserData(username);
+            return RedirectToAction("Details");
         }
     }
 }
